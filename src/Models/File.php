e52c12cebc3385463100197d8db91be9b5f402d3
Lebedev Vadim <v.lebedev@qsoft.ru>
2022-12-15 16:37:30 +0800

    Add getcloudpath method to File model

--- src/Models/File.php
+++ src/Models/File.php
@@ -4,10 +4,22 @@ namespace Awesome\Filesystem\Models;
 
 use Awesome\Foundation\Traits\Models\AwesomeModel;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class File extends Model
 {
     public $incrementing = false;
     protected $keyType = 'uuid';
     protected $guarded = [];
+
+    public function getCloudPath(): string
+    {
+        $path = '/' . trim($this->path, '/');
+
+        if (Storage::getDefaultDriver() === 'minio') {
+            $path = parse_url(Storage::disk('minio')->getDriver()->publicUrl($path))['path'] ?? $path;
+        }
+
+        return $path;
+    }
 }
